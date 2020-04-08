409c65854b0a87d269bd3f69fa31a04383f6e18a
ncvetic <cuekisa@yahoo.com>
2020-04-08 23:10:48 +0200

    a

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,7 @@
+import '../styles/styles.css'
+import 'lazysizes'
+
+import StickyHeader from './modules/StickyHeader'
+
+new StickyHeader()
+
---
+++ app/assets/scripts/modules/StickyHeader.js
@@ -0,0 +1,16 @@
+import throttle from 'lodash/throttle'
+import debounce from 'lodash/debounce'
+
+class StickyHeader {
+  constructor() {
+    this.events()
+  }
+
+  events() {
+    // alert("s");
+  }
+
+
+}
+
+export default StickyHeader
