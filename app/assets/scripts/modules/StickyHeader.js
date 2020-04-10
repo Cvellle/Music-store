5d0a6c0b764ed0562e2d484b4c508d077c2da1fc
ncvetic <cuekisa@yahoo.com>
2020-04-10 23:25:54 +0200

    all

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,5 +1,5 @@
-import throttle from 'lodash/throttle'
-import debounce from 'lodash/debounce'
+// import throttle from 'lodash/throttle'
+// import debounce from 'lodash/debounce'
 
 class StickyHeader {
   constructor() {
@@ -8,10 +8,20 @@ class StickyHeader {
 
   events() {
     const navEl = document.querySelector(".header__nav");
-    const menuIcon = document.querySelector(".header__menu-icon");
-    
-    menuIcon.onclick = () => navEl.classList.toggle("opened");   
+    const menuIcon = document.querySelector(".header__menu-icon");   
+    const navLinks = document.querySelectorAll(".opened > a");
+
+    menuIcon.onclick = () => navEl.classList.toggle("opened"); 
+
+    navLinks.forEach(addEventListener("click", closeNav));
+    function closeNav(e) {
+      e.target.parentNode.classList.remove("opened");
+    }
+
+    // window.addEventListener("scroll", ()=> {});
   }
+
+  
 
 
 }
