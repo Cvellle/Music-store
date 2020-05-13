83cf6828faf922e120d398c95689da2f391d732e
ncvetic <cuekisa@yahoo.com>
2020-05-13 22:03:25 +0200

    loadash import removed

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,18 +1,15 @@
-// import throttle from 'lodash/throttle'
-// import debounce from 'lodash/debounce'
-
 class StickyHeader {
-    constructor() { 
-      this.navLinks = document.querySelectorAll(".header__nav > a")
-      this.menuIcon = document.querySelector(".header__menu-icon")  
-      this.navEl = document.querySelector(".header__nav")
-      this.headerEl = document.querySelector(".header")
-      this.headerChildren = document.querySelector(".header > *")
-      this.events()
+  constructor() {
+    this.navLinks = document.querySelectorAll(".header__nav > a")
+    this.menuIcon = document.querySelector(".header__menu-icon")
+    this.navEl = document.querySelector(".header__nav")
+    this.headerEl = document.querySelector(".header")
+    this.headerChildren = document.querySelector(".header > *")
+    this.events()
   }
 
   events() {
-    window.addEventListener("scroll", () =>  this.navOnScroll());
+    window.addEventListener("scroll", () => this.navOnScroll());
     this.navLinks.forEach(el => el.addEventListener("click", () => this.toggleNav()))
     this.menuIcon.addEventListener("click", () => this.toggleNav())
   }
@@ -25,14 +22,12 @@ class StickyHeader {
     if (window.scrollY > this.headerEl.offsetHeight * 2) {
       this.headerEl.classList.add("smaller")
       this.headerChildren.classList.add("adjusted")
-      this.headerEl.style.transition = "1s";    
-    } else {    
-      this.headerEl.classList.remove("smaller")     
+      this.headerEl.style.transition = "1s";
+    } else {
+      this.headerEl.classList.remove("smaller")
       this.headerChildren.classList.remove("adjusted")
     }
   }
-
-
 }
 
 export default StickyHeader
